37ed63d9f2c2b3051935715b7bd3d57c03f06add
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-05-14 21:20:07 +0900

    iterative version

--- 01/ex_1_11.rkt
+++ 01/ex_1_11.rkt
@@ -1,7 +1,15 @@
 #lang racket
-(provide f)
-(define (f n)
+(provide f-recursive f-iterative)
+(define (f-recursive n)
     (cond ((< n 3) 3)
-        (else (+ (f (- n 1))
-            (* 2 (f (- n 2)))
-            (* 3 (f (- n 3)))))))
+        (else (+ (f-recursive (- n 1))
+            (* 2 (f-recursive (- n 2)))
+            (* 3 (f-recursive (- n 3)))))))
+
+(define (f-iterative n)
+    (f-iterative-iter 3 3 3 3 n))
+
+(define (f-iterative-iter fMinus1 fMinus2 fMinus3 cnt n)
+    (cond ((< n 3) 3)
+        ((> cnt n) fMinus1)
+        (else (f-iterative-iter (+ fMinus1 (* 2 fMinus2) (* 3 fMinus3)) fMinus1 fMinus2 (+ cnt 1) n))))
--- 01/ex_1_11_test.rkt
+++ 01/ex_1_11_test.rkt
@@ -2,6 +2,10 @@
 (require "../tools/test/testing.rkt")
 (require "./ex_1_11.rkt")
 
-(assert-equal "n = 2" (f 2) 3)
-(assert-equal "n = 3" (f 3) 18)
-(assert-equal "n = 4" (f 4) 33)
+(assert-equal "(recursive) n = 2" (f-recursive 2) 3)
+(assert-equal "(recursive) n = 3" (f-recursive 3) 18)
+(assert-equal "(recursive) n = 4" (f-recursive 4) 33)
+
+(assert-equal "(iterative) n = 2" 3 (f-iterative 2))
+(assert-equal "(iterative) n = 3" 18 (f-iterative 3))
+(assert-equal "(iterative) n = 4" 33 (f-iterative 4))
