d77d02c7d99df16b892d3bd6dc8a515bb04538ae
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-05-29 23:35:52 +0900

    1.36

--- 01/ex_1_35.rkt
+++ 01/ex_1_35.rkt
@@ -7,6 +7,8 @@
         )
     )
     (define (try guess)
+        (display guess)
+        (newline)
         (let ((next (f guess)))
             (if (close-enough? guess next)
                 next
@@ -17,4 +19,17 @@
     (try first-guess)
 )
 
+; 1.35
 (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+; 1.36
+; w/o average damping
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 3.0)
+; 33 lines
+
+; w average damping
+(define (average a b)
+    (/ (+ a b) 2)
+)
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 3.0)
+; 9 lines
