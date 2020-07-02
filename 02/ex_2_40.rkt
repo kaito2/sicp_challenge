42e8905be78ad462588201aee73dc01ff0e9b1f3
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-07-03 00:29:44 +0900

    2.40

---
+++ 02/ex_2_40.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+; `end` must be greater than or equal to `start`
+(define (enumerate-interval start end)
+    (cond ((> start end) null)
+        ((= start end) (list end))
+        (else (cons start (enumerate-interval (+ start 1) end)))))
+
+; (enumerate-interval 1 10)
+
+(define null '())
+(define (accumulate op init sequence)
+    (if (null? (cdr sequence))
+        (car sequence) 
+        (op (car sequence) (accumulate op init (cdr sequence)))))
+
+; (accumulate + 0 (list 1 2 3 4 5))
+
+(define (unique-pairs n)
+    (accumulate
+        append
+        '()
+        (map 
+            (lambda (x) 
+                (map 
+                    (lambda (y) (cons x y))
+                    (enumerate-interval 1 (- x 1))))
+            (enumerate-interval 1 n))))
+
+; (unique-pairs 10)
+(require "../01/smallest-divisor.rkt")
+(define (prime-sum? pair)
+    (prime? (sum-of-pair pair)))
+(define (sum-of-pair pair)
+    (+ (car pair) (cdr pair))
+)
+
+(accumulate 
+    ; judge prime?(x.i + x.j)
+    (lambda (x y) 
+        (if (prime-sum? x)
+            (cons (list (car x) (cdr x) (sum-of-pair x)) y)
+            y
+        )
+    )
+    null 
+    (unique-pairs 10)
+)
